173226deed21979185a34213ac2a984532173823
CLS11 <crystall.101600@gmail.com>
2024-10-06 21:15:06 +0530

    Added blend mode for the mixed color particles.

--- lib/custom_painter.dart
+++ lib/custom_painter.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'particle.dart';
+import 'package:generative_art/particle.dart';
 
 Offset polarToCartesian(double speed, double theta) {
   return Offset(speed * cos(theta), speed * sin(theta));
@@ -13,34 +13,18 @@ class Painter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    // Clear the canvas
-    canvas.clearRect(Rect.fromLTRB(0, 0, size.width, size.height), Colors.white);
-    
-    for (var p in particles) {
-      var velocity = polarToCartesian(p.speed, p.theta);
-      p.position += velocity;
-
-      // Check boundaries and reinitialize if necessary
-      if (p.position.dx < 0 || p.position.dx > size.width) {
-        p.position = Offset(rgn.nextDouble() * size.width, p.position.dy);
-      }
-      if (p.position.dy < 0 || p.position.dy > size.height) {
-        p.position = Offset(p.position.dx, rgn.nextDouble() * size.height);
-      }
-
-      // Paint the particle
-      final paint = Paint()..color = p.color;
-      canvas.drawCircle(p.position, p.radius, paint);
-    }
-
-    var center = Offset(size.width / 2, size.height / 2);
-    var centerRadius = 100.0;
-    var centerPaint = Paint()..color = Colors.blueGrey;
-    canvas.drawCircle(center, centerRadius, centerPaint);
+    particles.forEach(
+      (p) {
+        var paint = Paint();
+        paint.blendMode = BlendMode.modulate;
+        paint.color = p.color;
+        canvas.drawCircle(p.position, p.radius, paint);
+      },
+    );
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true; 
+  bool shouldRepaint(CustomPainter o) {
+    return true;
   }
 }
